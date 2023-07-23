899ef865be2a6fdcc4a6135d08d7cb24ad035b94
Ratio <31981754+4F6D@users.noreply.github.com>
2023-07-23 21:20:37 +0200

    Initial Commit

---
+++ Shared/OpenGLApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenGLApp.swift
+//  Shared
+//
+//  Created by Rene on 23.07.23.
+//
+
+import SwiftUI
+
+@main
+struct OpenGLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
